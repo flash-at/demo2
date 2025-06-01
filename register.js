4ca9369c8178d737332eaede51fd5a5eeaf3728d
Mahesh <152671168+Mahesh-ch06@users.noreply.github.com>
2025-06-01 21:22:32 +0530

    Create register.js

---
+++ register.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyD1iij4QWlxQJJPS-yJrhSiCS79kS4dqaM",
+    authDomain: "portfolio-56be7.firebaseapp.com",
+    projectId: "portfolio-56be7",
+    storageBucket: "portfolio-56be7.firebasestorage.app",
+    messagingSenderId: "888511551571",
+    appId: "1:888511551571:web:11e809e995377e9a4ccea6",
+    measurementId: "G-X3CYL9YZR1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
